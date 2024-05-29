f2179cfad8ca9df6c0b725b0ab2730b6ffde4d81
Paweł Bornikowski <pawel.bornikowski@gmail.com>
2024-05-29 20:33:00 +0200

    feat: add navbar and sign-in flow

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Sign } from "crypto";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full flex justify-between items-center px-4 sm:px-24 z-50 h-12 bg-zinc-300">
+      <div>
+        <span>My-Amigo</span>
+      </div>
+      <div className=" flex-center gap-4">
+        <span>Create</span>
+        <span>Storage</span>
+        <span className="flex items-center">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
