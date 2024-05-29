0107de323fdc65af6b0a53ecba69c043543ef0f6
Paweł Bornikowski <pawel.bornikowski@gmail.com>
2024-05-29 21:55:04 +0200

    styles: add sudo class for bg text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <h1 className="flex flex-col text-5xl gap-5 font-bold">
           <span>Create your Amigo,</span>
           <span>
-            share it <span className="bg-yellow px-2">everywhere</span>
+            share it <span className="px-2 hero_text">everywhere</span>
           </span>
         </h1>
         <p className="text-balance">
