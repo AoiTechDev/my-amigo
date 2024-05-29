52adc31133074127ebd892b6335382bac63cbb59
Paweł Bornikowski <pawel.bornikowski@gmail.com>
2024-05-29 21:36:28 +0200

    feat: create header, subheader and CTA

---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="min-h-screen  max-w-6xl w-full mx-auto flex items-center">
+      <div className="space-y-10 flex-1">
+        <h1 className="flex flex-col text-5xl gap-5 font-bold">
+          <span>Create your Amigo,</span>
+          <span>
+            share it <span className="bg-yellow px-2">everywhere</span>
+          </span>
+        </h1>
+        <p className="text-balance">
+          Combine favorite frameworks with fun accessories to craft a unique,
+          funny avatar. Perfect for showing off on social media and profiles!
+        </p>
+        <button className="bg-yellow px-12 py-3 rounded-lg">
+          Create Amigo
+        </button>
+      </div>
+      <div className="flex-1"></div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full flex justify-between items-center px-4 sm:px-24 z-50 h-12 bg-zinc-300">
+    <nav className="fixed top-0 w-full flex justify-between items-center px-4 sm:px-24 z-50 h-12 ">
       <div>
         <span>My-Amigo</span>
       </div>
       <div className=" flex-center gap-4">
         <span>Create</span>
         <span>Storage</span>
+      </div>
+      <div>
         <span className="flex items-center">
           <SignedIn>
             <UserButton />
